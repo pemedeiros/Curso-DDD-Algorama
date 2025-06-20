c9c1e1bfccc7599dc7b2b95b5f37001f6f7848e2
Pedro Augusto Braga Medeiros <pedro.medeiros@demarco.com.br>
2025-06-20 19:23:56 -0300

    Inicializa o modelo das entidades.

---
+++ Churras/Empresa.Churras.Domain.Model/Entities/Colega.cs
@@ -0,0 +1,13 @@
+﻿using Kernel.Domain.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Empresa.Churras.Domain.Model.Entities
+{
+    public class Colega :  EntityKeySeq
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+    }
+}
